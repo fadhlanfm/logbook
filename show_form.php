a21832c2c8f8514434528a820d7e757b3d595bb7
caesarah <caesargilang@gmail.com>
2016-07-27 08:52:11 +0700

    modified layout show_form.php

--- show_form.php
+++ show_form.php
@@ -18,9 +18,43 @@
 <html>
 <head>
 	<title>Show Log Book</title>
+	<meta charset="UTF-8">
+	  <!--Import Google Icon Font-->
+	  <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="materialize/css/googlefont.css"  media="screen,projection"/>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="materialize/css/materialize.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="materialize/js/jquery-3.0.0.min.js"></script>
+    
+    <!-- Compiled and minified CSS -->
+	  <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+	  <!-- Compiled and minified JavaScript -->
+    <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+	  <script src="/pickadate.js-3.5.6/lib/picker.js"></script>
+	  <script src="/pickadate.js-3.5.6/lib/picker.date.js"></script>
+	  <script src="/pickadate.js-3.5.6/lib/picker.time.js"></script>
+	  <script>
+      window.liveSettings = {
+        api_key: "a0b49b34b93844c38eaee15690d86413",
+        picker: "bottom-right",
+        detectlang: true,
+        dynamic: true,
+        autocollect: true
+      };
+    </script>
+    <script type="text/javascript" src="materialize/js/live.js"></script>
+    <script type="text/javascript" src="/leanModal.v1.1/jquery.leanModal.min.js"></script>
 </head>
 <body>
-	<table border="1">
+<div class="container">
+<div class="center">
+	<h1>Logbook</h1>
+</div>
+	<table class="striped">
 		<tr>
 			<th>Nomor</th>
 			<th>Kode Unit</th>
@@ -29,7 +63,7 @@
 			<th>Mulai Program</th>
 			<th>Berakhir Program</th>
 			<th>Status</th>
-			<th colspan="3">Aksi</th>
+			<th colspan="3" class="center">Aksi</th>
 			<th colspan="3">Ubah Status</th>			
 		</tr>
 		<?php
@@ -58,11 +92,12 @@
 				echo'</tr>';
 				$i++;
 			}
-			echo'<br />';
-			echo'Total Rows = '.$result->num_rows;
+			echo'<br> <br>';
+			// echo'Total Rows = '.$result->num_rows;
 			$result->free();
 			$db->close();
 		?>
 	</table>
+	</div><br>
 </body>
 </html>
