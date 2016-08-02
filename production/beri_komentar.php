1984c0816b706edf3fb503465d8cb51651102a55
caesarah <caesargilang@gmail.com>
2016-08-02 10:42:31 +0700

    banyak

--- production/beri_komentar.php
+++ production/beri_komentar.php
@@ -125,7 +125,7 @@ else
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt=""><?php echo''.$row2->name.''; ?>
+                    <img src="images/img.jpg" alt=""><?php echo''.$row2->username.''; ?>
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
