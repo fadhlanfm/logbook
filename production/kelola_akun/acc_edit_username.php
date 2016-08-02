f6f00f2635c8d4804f1cbadd2c9dcc395e5704c1
fadhlanfm <fafadhlanriz@gmail.com>
2016-08-02 16:54:40 +0700

    edit username

---
+++ production/kelola_akun/acc_edit_username.php
@@ -0,0 +1,17 @@
+<?php
+	$iduser=$_POST['iduser'];
+	$username=$_POST['username'];
+	include('../connect_db.php');
+	$query = "UPDATE user SET username='$username' WHERE iduser=$iduser";
+		$result = $db->query($query);
+		if(!$result)
+		{
+			die("Could not query the database: <br />". $db->error);
+		}else
+		{
+			echo 'Data Updated. </br>';
+			echo '<a href="	read_account.php">Lihat</a>';
+			$db->close();
+			exit;
+		}	
+?>
