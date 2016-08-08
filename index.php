31691aa0aa06b1fa3eaa033e2774d5c0929be91d
caesarah <caesargilang@gmail.com>
2016-08-08 15:59:52 +0700

    removed unused files

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include('process/connect_db.php');
 $db = new mysqli($db_host,$db_username, $db_password, $db_database);
 if ($db->connect_errno)
 {
-  die("Could not connect to teh database: <br />".$db->connect_error);
+  die("Could not connect to the database: <br />".$db->connect_error);
 }
 
 if(isset($_SESSION['role']) && $_SESSION['role'] == -1)
